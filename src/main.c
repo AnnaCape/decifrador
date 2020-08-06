b24a80c6301c33c0cfb5f9af7f299f4cfcf59561
AnnaCape <61944693+AnnaCape@users.noreply.github.com>
2020-08-05 21:04:33 -0300

    listo

--- src/main.c
+++ src/main.c
@@ -6,14 +6,29 @@
 
 #include <stdio.h>
 
-#define TAM_BUFFER 50
+#define TAM_BUFFER 60 //aumentei , ficou pequeno para o test 2
 char buffer_entrada[TAM_BUFFER];
 char buffer_saida[TAM_BUFFER];
+char senha[]="SENHASECRETA";
 
+char decifrar(char entrada, char senha) {
+  char saida, a, b;
+   if (entrada>='A' && entrada<='Z') {
+    a = entrada - 'A';
+    b = senha - 'A';
+    saida = 'A'+( a - b + 26)% 26;
+	return saida;
+   } else return entrada;
+}
 int main() {
   /* Leitura da entrada */
   fgets(buffer_entrada, TAM_BUFFER, stdin);
-
+  int i_senha=0;
+	for (int i=0;(i<TAM_BUFFER) && (buffer_entrada[i]!='\0') && (buffer_entrada[i]!='\n');i++){
+		buffer_saida[i]=decifrar(buffer_entrada[i], senha[i_senha]);
+		i_senha++;
+		if (senha[i_senha]=='\0') i_senha=0;
+	}
 
 
   /* Escrever saida na tela */
